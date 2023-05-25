597697820cbd240f99f3009ab7c6dfed78be7138
Oliver Carvajal <oliver.carvajal@aerolab.co>
2023-05-25 17:20:39 -0500

    chore: swith to regular Page folder instead of App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { SWRConfig } from "swr";
+import "../styles/globals.css";
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig value={{ fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()) }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
+}
